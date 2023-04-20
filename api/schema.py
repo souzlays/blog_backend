761cb19d65089cf2ce16998bd6d4a1b4e73934a5
souzlays <souzalaayss@gmail.com>
2023-04-20 00:59:08 -0300

    inclui registro de post

--- api/schema.py
+++ api/schema.py
@@ -18,3 +18,19 @@ class Query:
     def resolve_posts(self,info,**kwargs):  
         return Post.objects.filter(**kwargs)                                
 
+
+class CreatPost(graphene.relay.ClientIDMutation):
+    post= graphene.Field(PostData)
+    class Input:
+        title= graphene.String()
+        body= graphene.String()
+        user= graphene.Int()
+    def  mutate_and_get_payload(self,info,**kwargs):
+        title= kwargs.get("title") 
+        body= kwargs.get("body")
+        user= kwargs.get("user")    
+        post= Post.objects.create(title=title, body=body, user=user)
+        return CreatPost(post)
+
+class Mutation:
+    create_post= CreatPost.Field()
--- blog/schema.py
+++ blog/schema.py
@@ -5,6 +5,7 @@ import api.schema
 
 class Query(api.schema.Query, graphene.ObjectType):
     pass
+class Mutation(api.schema.Mutation, graphene.ObjectType):
+    pass
 
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query,mutation= Mutation)
