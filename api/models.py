a6693ab39785fd151fef9c7a567a6cb112b0c146
souzlays <souzalaayss@gmail.com>
2023-04-20 00:10:30 -0300

    inclui consulta de post

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post (models.Model):
+    title= models.CharField(max_length=100, null=False,blank=False)
+    body= models.TextField()
+    user= models.IntegerField(null=False)
+    date= models.DateTimeField(auto_now_add=True)
+    
--- api/schema.py
+++ api/schema.py
@@ -1,7 +1,20 @@
 import graphene
 from django.conf import settings
+from api.models import Post
+
+
+class PostData(graphene.ObjectType):
+    title= graphene.String()
+    body= graphene.String()
+    user= graphene.Int()
+    date= graphene.DateTime()
 
 class Query:
     version= graphene.String ()
     def resolve_version(self,info,**kwargs):
         return settings.VERSION
+
+    posts= graphene.List(PostData)
+    def resolve_posts(self,info,**kwargs):  
+        return Post.objects.filter(**kwargs)                                
+
